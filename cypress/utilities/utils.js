7fbe1d668da889c2928156ef02c727a46d0e8e88
md shahnewaz zaman <mzaman@resqsoft.com>
2023-05-25 11:13:04 -0400

    nightly build, codeownersection, pull request template

--- cypress/utilities/utils.js
+++ cypress/utilities/utils.js
@@ -17,12 +17,5 @@ const utils = {
     verifyLinkValues: (locator, haveAttr, href, linkValue) => {
         cy.get(locator).should(haveAttr, href, linkValue);
     }
-    /*
-    name: Deploy Nightly
-on:
-  schedule:
-    - cron: '0 2 * * *' # run at 2 AM UTC
-
-    */
 }
 module.exports = utils;
